d89eee301580b92cb3b3b923f59a17feca4ca05c
Mark Wilkinson <markw@illuminae.com>
2020-07-21 11:58:17 +0200

    removed superfluous cd for dicker start

--- start_R.sh
+++ start_R.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker run --rm  -d -p 9999:9999 -v /home/osboxes/R-SHARED:/R-SHARED --name r-upm-masters markw/r-upm-masters:latest
-cd /home/osboxes/R-SHARED
 sleep 2
 firefox http://localhost:9999
