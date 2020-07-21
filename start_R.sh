1584e6f384e027a5523c7fc416a604b946e94e83
Mark Wilkinson <markw@illuminae.com>
2020-07-21 11:51:33 +0200

    removed superfluous flags for dicker start

--- start_R.sh
+++ start_R.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --rm -it -d -p 9999:9999 -v /home/osboxes/R-SHARED:/R-SHARED --name r-upm-masters markw/r-upm-masters:latest
+docker run --rm  -d -p 9999:9999 -v /home/osboxes/R-SHARED:/R-SHARED --name r-upm-masters markw/r-upm-masters:latest
 cd /home/osboxes/R-SHARED
 sleep 2
 firefox http://localhost:9999
